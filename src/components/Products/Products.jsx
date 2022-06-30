464f54533ee7d9bfc666a844f375554e7b3c4dd1
acacia <acidicrain@acidicrain.local>
2022-06-29 21:19:37 -0400

    passed products as props, updated props locations for images, prices, and description.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'PERLA Earrings', description: 'Fille with a pearl earring - thanks to these fine gold beauties.', price: '$110', image: 'https://i.postimg.cc/J0g60BHF/59-DEC0-B8-5019-4650-BA95-D9161-C2-A7-D2-F-1-201-a.jpg' },
-    { id: 2, name: 'AMELIE Necklace', description: 'A heart of gold... literally.', price: '$185', image: 'https://i.postimg.cc/zGY7QNCT/D18-F75-D0-40-EA-4451-B492-0167-FC7290-E9.png' },
-    { id: 3, name: 'AUDREY Earrings', description: 'Directly from a family jewel box. Here is Audrey: the twisted loops that we do not want to take off.', price: '$110', image: 'https://i.postimg.cc/pVg3PgQL/47-BE3-CF6-4118-4-EE7-B5-A9-EDCDC2-DAD8-AB.png' },
-];
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
